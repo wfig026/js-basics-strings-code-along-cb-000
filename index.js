ade5eb81c5bdc02817e02d87e2090cebe27a5572
Will Figueiredo <will@wfigueiredo.com>
2019-03-27 19:22:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 // Write your code in this file!
 const currentUser = "Grace Hopper";
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+const excitedWelcomeMessage = welcomeMessage.toUpperCase;
